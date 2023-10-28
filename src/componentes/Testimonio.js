965bc4dbb0c9d040f9ea0279ad2e26ea4561a63e
Martin Conenna <117860765+martinredondo89@users.noreply.github.com>
2023-10-28 01:55:12 -0300

    se agrego estilo

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import '../hojas-de-estilo/Testimonio.css';
 
+//Componente funcional
 function Testimonio() {
   return (
     <div className='contenedor-testimonio'>
@@ -7,7 +9,17 @@ function Testimonio() {
         className='imagen-testimonio'
         src={require('../imagenes/testimonio-emma.png')}
         alt='Foto de Emma' />
-        
+      <div className="contenedor-texto-testimonio">
+        <p classname="nombre-testimonio">Emma Bostian en Suecia</p>
+        <p classname="cargo-testimonio">Ingeniera de Software en Spotify</p>
+        <p classname="texto-testimonio">Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.</p>
+      </div>
+
     </div>
   )
 }
+//Exportacion por defecto, Este va a ser el unico componente de este archivo.
+export default Testimonio;
+
+//La exportacion nombrada se agrega export delante de cada componente
+// y la importacion se hace agregando llaves.
