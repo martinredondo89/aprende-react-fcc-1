745344476c5ce261a64300d23519f62975bfe71c
Martin Conenna <117860765+martinredondo89@users.noreply.github.com>
2023-10-29 16:03:12 -0300

    Solo faltan los retos

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -2,17 +2,19 @@ import React from 'react';
 import '../hojas-de-estilo/Testimonio.css';
 
 //Componente funcional
-function Testimonio() {
+function Testimonio(props) {
+  
   return (
+    //Con las plantillas literales podemos usar js dentro
     <div className='contenedor-testimonio'>
       <img
         className='imagen-testimonio'
-        src={require('../imagenes/testimonio-emma.png')}
-        alt='Foto de Emma' />
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt={`Foto de ${props.nombre}`} />
       <div className="contenedor-texto-testimonio">
-        <p classname="nombre-testimonio">Emma Bostian en Suecia</p>
-        <p classname="cargo-testimonio">Ingeniera de Software en Spotify</p>
-        <p classname="texto-testimonio">Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.</p>
+        <p className="nombre-testimonio"><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
 
     </div>
@@ -23,3 +25,5 @@ export default Testimonio;
 
 //La exportacion nombrada se agrega export delante de cada componente
 // y la importacion se hace agregando llaves.
+
+// ***** Reto : freecode en negrita y cargos los props desde un archivo
