a188b4d6ae736b553ffbb14eb6d1e30bed415e3a
Martin Conenna <117860765+martinredondo89@users.noreply.github.com>
2023-11-22 22:57:08 -0300

    clase en vez de componente

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import '../hojas-de-estilo/Testimonio.css';
+//Componente clase * this.props.nombre*
+class Testimonio extends React.Component {
+  render() {
+    return (
+      <div className='contenedor-testimonio'>
+        <img
+          className='imagen-testimonio'
+          src={require(`../imagenes/testimonio-${this.props.imagen}.png`)}
+          alt={`Foto de ${this.props.nombre}`} />
+        <div className="contenedor-texto-testimonio">
+          <p className="nombre-testimonio"><strong>{this.props.nombre}</strong> en {this.props.pais}</p>
+          <p className="cargo-testimonio">{this.props.cargo} en <strong>{this.props.empresa}</strong></p>
+          <p className="texto-testimonio">"{this.props.testimonio}"</p>
+        </div>
 
-//Componente funcional
+      </div>
+    )
+  }
+}
+
+/*Componente funcional
 function Testimonio(props) {
-  
   return (
     //Con las plantillas literales podemos usar js dentro
     <div className='contenedor-testimonio'>
@@ -20,6 +38,9 @@ function Testimonio(props) {
     </div>
   )
 }
+*/
+
+
 //Exportacion por defecto, Este va a ser el unico componente de este archivo.
 export default Testimonio;
 
